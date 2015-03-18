3741fd80cfab21724c88e2c1cd9afcaab404c715
J4r3tt <zyrdyx@gmail.com>
2015-03-18 18:38:48 +0800

    milestone common sms done

--- sms.py
+++ sms.py
@@ -15,9 +15,9 @@ def bcdDigits(chars):
         result.append(hi)
     return "".join([str(v) for v in result])
 
+
 class GSMTAP:
     # gsmtap http://bb.osmocom.org/trac/attachment/wiki/GSMTAP/gsmtap.h
-    
 
     def __init__(self, gsmtap):
         self.gsmtap = gsmtap
@@ -34,7 +34,7 @@ class GSMTAP:
         self.channel_type = ord(gsmtap[12])
         self.antenna_number = ord(gsmtap[13])
         self.sub_slot = ord(gsmtap[14])
-        self.next_data=self.gsmtap[self.hdr_len:]
+        self.next_data = self.gsmtap[self.hdr_len:]
 
     # def get_payload(self):
         # return self.gsmtap[self.hdr_len:]
@@ -45,7 +45,7 @@ class LAPDm:
     # base on wireshark gsmtap decode
 
     def __init__(self, lapdm):
-        self.lapdm=lapdm
+        self.lapdm = lapdm
         self.addr_field = ord(lapdm[0])
         self.lpd = (ord(lapdm[0]) >> 5) & 0x3
         self.sapi = (ord(lapdm[0]) >> 2) & 0x7
@@ -56,33 +56,35 @@ class LAPDm:
         self.len_field = ord(lapdm[2])
         self.last_segment = (ord(lapdm[2]) >> 1) & 0x1
         self.length = ord(lapdm[2]) >> 2
-        self.next_data=self.lapdm[3:]
+        self.next_data = self.lapdm[3:]
     # def get_data(self):
         # return self.lapdm[3:]
+
 
 class DTAP:
 
     def __init__(self, dtap):
-        self.dtap= dtap
+        self.dtap = dtap
         self.protocol_discriminator = ord(dtap[0:1]) & 0xF
         self.dtap_sms_type = ord(dtap[1])
-        self.cp_lenth=ord(dtap[2])
-        self.next_data=dtap[3:]
+        self.cp_lenth = ord(dtap[2])
+        self.next_data = dtap[3:]
+
 
 class RP:
 
     def __init__(self, rp):
         self.rp = rp
-        self.RP_message_type=ord(rp[0])
+        self.RP_message_type = ord(rp[0])
         self.RP_origin_len = ord(rp[2])
         self.RP_origin_ext = ord(rp[3])
         self.RP_origin = bcdDigits(rp[4:3 + self.RP_origin_len])
-        self.RP_dest_start = 3+ self.RP_origin_len
+        self.RP_dest_start = 3 + self.RP_origin_len
         self.RP_dest_len = ord(rp[self.RP_dest_start])
         self.RP_dest_over = self.RP_dest_start + self.RP_dest_len + 1
         self.length = ord(rp[self.RP_dest_over])
         self.tpdu_off = self.RP_dest_over + 1
-        self.next_data=self.rp[self.tpdu_off:self.tpdu_off+self.length]
+        self.next_data = self.rp[self.tpdu_off:self.tpdu_off + self.length]
     # def get_tpdu(self):
     #     return self.rp[self.tpdu_off:self.tpdu_off+self.length]
 
@@ -90,15 +92,21 @@ class RP:
 class TPDU:
 
     def __init__(self, tpdu):
-
-        # SMS-DELIVER
+        self.TP_udhi = (ord(tpdu[0]) >> 6) & 0x01
+        self.TP_mti = ord(tpdu[0]) & 0x03
+        self.TP_mms = (ord(tpdu[0]) >> 2) & 0x01
         self.TP_origin_num = ord(tpdu[1])
-        self.TP_origin_len = (self.TP_origin_num >> 1) + (self.TP_origin_num % 2)
+        self.TP_origin_len = (
+            self.TP_origin_num >> 1) + (self.TP_origin_num % 2)
         self.TP_origin_ext = ord(tpdu[2])
         self.TP_origin = bcdDigits(tpdu[3:3 + self.TP_origin_len])
         self.data_start = 3 + self.TP_origin_len + 9
         self.tpu_len = ord(tpdu[self.data_start])
-        self.data = tpdu[self.data_start + 1:self.data_start + 1 + self.tpu_len]
-
-    def get_data(self):
-        return self.data.decode("utf-16be").encode("utf-8")
+        print self.TP_udhi
+        if self.TP_udhi == 0:
+            self.data = tpdu[
+                self.data_start + 1:self.data_start + 1 + self.tpu_len]
+        else:
+            self.userdata_len = ord(tpdu[self.data_start+1])
+            self.data = tpdu[
+                self.data_start + 2 + self.userdata_len:self.data_start + 2 + self.userdata_len + self.tpu_len]
--- sniff.py
+++ sniff.py
@@ -15,23 +15,35 @@ def handle_message(**kargs):
     while True:
         data = kargs['messages'].get(True)
         gsmtap = GSMTAP(data)
-        if gsmtap.version==2:
+        if gsmtap.version == 2:
             lapdm = LAPDm(gsmtap.next_data)
-            # SAPI: 3 - Short message service && frame type is 0x00
+            # sapi is 3-sms && frame type is 0x00
             if (gsmtap.channel_type == 8) and (lapdm.sapi == 3) and (lapdm.frame_type == 0x00):
-                sms_segments+=lapdm.next_data
-                if lapdm.last_segment==0:
-                    reassembled_segments=sms_segments
-                    sms_segments=""
-                    dtap=DTAP(reassembled_segments)
-                    if (dtap.protocol_discriminator==0x09) and (dtap.dtap_sms_type==0x01) and (dtap.cp_lenth>0x10) and len(reassembled_segments)>10:
-
-
-                    #hexdump.hexdump(lapdm.next_data)
-                        rp=RP(dtap.next_data)
-                        tpdu=TPDU(rp.next_data)
-                #print rp.RP_origin
-                print("[SMS from %s] %s" % (tpdu.TP_origin, tpdu.get_data()))     
+                # put the packets together
+                sms_segments += lapdm.next_data
+                # if last packet
+                if lapdm.last_segment == 0:
+                    # ready to handle this segment
+                    reassembled_segments = sms_segments
+                    sms_segments = ""
+                    dtap = DTAP(reassembled_segments)
+                    if (dtap.protocol_discriminator == 0x09) and (dtap.dtap_sms_type == 0x01) and (dtap.cp_lenth > 0x10) \
+                            and len(reassembled_segments) > 10:
+                        # hexdump.hexdump(lapdm.next_data)
+                        rp = RP(dtap.next_data)
+                        # dowanlink
+                        if rp.RP_message_type == 0x01:
+                            tpdu = TPDU(rp.next_data)
+                            # SMS-DELIVER
+                            print tpdu.TP_mti
+                            if tpdu.TP_mti == 0x00:
+                                # print rp.RP_origin
+                                print("[SMS from %s] %s" % (
+                                    tpdu.TP_origin, tpdu.data.decode("utf-16be").encode("utf-8")))
+                            if tpdu.TP_mti == 0x02:
+                                print("Downlink SMS status report")
+                        else:
+                            print ("Uplink SMS status report ")
                 # print("LINK[%d] ARFCN=%d TIME_SLOT=%d CHANNEL=%d, N(R)=%d N(S)=%d, segment more[%d], payload len=%d\n" %
                 #   (gsmtap.link, gsmtap.arfcn, gsmtap.time_slot, gsmtap.channel_type, lapdm.n_r, lapdm.n_s, lapdm.last_segment, lapdm.length))
 
@@ -39,15 +51,15 @@ if __name__ == '__main__':
     print "Sniffer Start..."
     print "Press Ctrl+C to Exit."
     try:
-        q = Queue.Queue()
-        t = threading.Thread(
+        q=Queue.Queue()
+        t=threading.Thread(
             target=handle_message, name="handle_message_thread", kwargs={'messages': q})
-        t.daemon = True
+        t.daemon=True
         t.start()
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(('0.0.0.0', 4729))
         while True:
-            udp_data, addr = s.recvfrom(2048)
+            udp_data, addr=s.recvfrom(2048)
             q.put(udp_data)
         s.close()
     except KeyboardInterrupt:
