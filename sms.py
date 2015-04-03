818382e45d375fa94dd725fa4261f20fc026e8cd
J4r3tt <zyrdyx@gmail.com>
2015-04-03 17:40:26 +0800

    final test

--- sms.py
+++ sms.py
@@ -55,7 +55,7 @@ class LAPDm:
         self.frame_type = ord(lapdm[1]) & 0x1
         self.len_field = ord(lapdm[2])
         self.last_segment = (ord(lapdm[2]) >> 1) & 0x1
-        self.length = ord(lapdm[2]) >> 2
+        self.length = (ord(lapdm[2]) >> 2) & 0x3f
         self.next_data = self.lapdm[3:]
     # def get_data(self):
         # return self.lapdm[3:]
@@ -76,27 +76,35 @@ class RP:
     def __init__(self, rp):
         self.rp = rp
         self.RP_message_type = ord(rp[0])
-        self.RP_origin_len = ord(rp[2])
-        self.RP_origin_ext = ord(rp[3])
-        self.RP_origin = bcdDigits(rp[4:3 + self.RP_origin_len])
-        self.RP_dest_start = 3 + self.RP_origin_len
-        self.RP_dest_len = ord(rp[self.RP_dest_start])
-        self.RP_dest_over = self.RP_dest_start + self.RP_dest_len + 1
-        self.length = ord(rp[self.RP_dest_over])
-        self.tpdu_off = self.RP_dest_over + 1
-        self.next_data = self.rp[self.tpdu_off:self.tpdu_off + self.length]
-    # def get_tpdu(self):
-    #     return self.rp[self.tpdu_off:self.tpdu_off+self.length]
+
+        # Message Type RP-DATA (Network to MS)
+        if self.RP_message_type == 0x01:
+            self.RP_origin_len = ord(rp[2])
+            self.RP_origin_ext = ord(rp[3])
+            self.RP_origin = bcdDigits(rp[4:3 + self.RP_origin_len])
+            self.length = ord(rp[3 + self.RP_origin_len + 1])
+            self.next_data = self.rp[3 + self.RP_origin_len + 2:]
+        # Message Type RP-DATA (MS to Network)
+        elif self.RP_message_type == 0x00:
+            self.RP_dest_len = ord(rp[3])
+            self.RP_dest_ext = ord(rp[4])
+            self.RP_dest = bcdDigits(rp[5:4 + self.RP_dest_len])
+            self.lenth = ord(rp[4 + self.RP_dest_len + 1])
+            self.next_data = self.rp[4 + self.RP_dest_len + 2:]
+       # Message Type RP-ACK (MS to Network)
+        elif self.RP_message_type == 0x02:
+            self.lenth = ord(rp[3])
+            self.next_data = self.rp[4:4 + self.lenth]
 
 
 class TPDU:
 
     def __init__(self, tpdu):
         self.TP_udhi = (ord(tpdu[0]) >> 6) & 0x01
-        self.TP_mms = (ord(tpdu[0]) >> 2) & 0x01
         self.TP_mti = ord(tpdu[0]) & 0x03
-        # SMS-DELIVER or SMS-SUBMIT
-        if (self.TP_mti == 0) or (self.TP_mti == 1):
+        # SMS-DELIVER
+        if self.TP_mti == 0:
+            self.TP_mms = (ord(tpdu[0]) >> 2) & 0x01
             self.TP_origin_num = ord(tpdu[1])
             self.TP_origin_len = (
                 self.TP_origin_num >> 1) + (self.TP_origin_num % 2)
@@ -104,15 +112,43 @@ class TPDU:
             self.TP_origin = bcdDigits(tpdu[3:3 + self.TP_origin_len])
             self.TP_charaterset = ord(
                 tpdu[3 + self.TP_origin_len + 1]) >> 2 & 0x03
+            self.time_stamp = bcdDigits(
+                tpdu[3 + self.TP_origin_len + 2:3 + self.TP_origin_len + 8])
             self.data_start = 3 + self.TP_origin_len + 9
             self.tpu_len = ord(tpdu[self.data_start])
+            #deal with long sms
             if self.TP_udhi == 0:
                 self.data = tpdu[
                     self.data_start + 1:self.data_start + 1 + self.tpu_len]
             else:
                 self.userdata_len = ord(tpdu[self.data_start + 1])
                 self.data = tpdu[
-                    self.data_start + 2 + self.userdata_len:self.data_start + 2 + self.userdata_len + self.tpu_len]
+                    self.data_start + 2 + self.userdata_len:self.data_start + 1 + self.tpu_len]
+        # SMS-SUBMIT
+        elif self.TP_mti == 1:
+            self.TP_vpf=(ord(tpdu[0]) >> 3) & 0x03
+            self.TP_dest_num = ord(tpdu[2])
+            self.TP_dest_len = (
+                self.TP_dest_num >> 1) + (self.TP_dest_num % 2)
+            self.TP_dest_ext = ord(tpdu[3])
+            self.TP_dest = bcdDigits(tpdu[4:4 + self.TP_dest_len])
+            self.TP_charaterset = ord(
+                tpdu[4 + self.TP_dest_len + 1]) >> 2 & 0x03
+            #if contain TP-Validity-Period header
+            if self.TP_vpf==2:
+                self.data_start = 4 + self.TP_dest_len + 3
+            else:
+                self.data_start = 4 + self.TP_dest_len + 2
+            self.tpu_len = ord(tpdu[self.data_start])
+            #deal with long sms
+            if self.TP_udhi == 0:
+                self.data = tpdu[
+                    self.data_start + 1:self.data_start + 1 + self.tpu_len]
+            else:
+                self.userdata_len = ord(tpdu[self.data_start + 1])
+                self.data = tpdu[
+                    self.data_start + 2 + self.userdata_len:self.data_start + 1 + self.tpu_len]
+
         # SMS-STATUS REPORT
         elif self.TP_mti == 2:
             self.TP_origin_num = ord(tpdu[2])
@@ -120,4 +156,6 @@ class TPDU:
                 self.TP_origin_num >> 1) + (self.TP_origin_num % 2)
             self.TP_origin_ext = ord(tpdu[3])
             self.TP_origin = bcdDigits(tpdu[4:4 + self.TP_origin_len])
-            self.status_result = ord(tpdu[4 + self.TP_origin_len + 14:]) & 0x7f
+            self.time_stamp = bcdDigits(
+                tpdu[4 + self.TP_origin_len:4 + self.TP_origin_len + 6])
+            self.status_result = ord(tpdu[4 + self.TP_origin_len + 6]) & 0x7f
--- sniff.py
+++ sniff.py
@@ -8,7 +8,7 @@ import Queue
 import time
 import subprocess
 from sms import *
-
+import hexdump
 
 def handle_message(**kargs):
     sms_segments = ""
@@ -27,32 +27,46 @@ def handle_message(**kargs):
                     reassembled_segments = sms_segments
                     sms_segments = ""
                     dtap = DTAP(reassembled_segments)
-                    if (dtap.protocol_discriminator == 0x09) and (dtap.dtap_sms_type == 0x01) and (dtap.cp_lenth > 0x10) \
-                            and len(reassembled_segments) > 10:
+                    #print dtap.cp_lenth
+                    #print len(reassembled_segments)
+                    if (dtap.protocol_discriminator == 0x09) and (dtap.dtap_sms_type == 0x01) and (dtap.cp_lenth > 2) \
+                            and len(reassembled_segments) > 8:
                         # hexdump.hexdump(lapdm.next_data)
                         rp = RP(dtap.next_data)
-                        print rp.RP_message_type
-                        print rp.RP_origin_len
+
                         # Message Type RP-DATA (Network to MS)
                         if rp.RP_message_type == 1:
                             tpdu = TPDU(rp.next_data)
+                            print "-"*90
                             # SMS-DELIVER
                             if tpdu.TP_mti == 0x00:
                                 # it's sms
                                 if tpdu.TP_charaterset == 2:
                                     try:
-                                        print("[SMS from %s] %s" % (
-                                            tpdu.TP_origin, tpdu.data.decode("utf-16be").encode("utf-8")))
+                                        center_time=time.strptime(tpdu.time_stamp,"%y%m%d%H%M%S")
                                     except:
-                                        print(
-                                            "Decode Failed!Maybe wrong format")
+                                        center_time=time.localtime()
+                                    #hexdump.hexdump(tpdu.data)
+                                    print("%s (Downlink) [SMS from %s] %s" % (
+                                        time.strftime("%Y-%m-%d %H:%M:%S", center_time),tpdu.TP_origin, tpdu.data.decode("utf-16be").encode("utf-8")))
+                                    # except:
+                                    #     print(
+                                    #         "(Downlink) Decode Failed!Maybe wrong format")
                                 # it's mmse
                                 elif tpdu.TP_charaterset == 1:
-                                    print("[MMSE from %s] " % (tpdu.TP_origin))
+                                    try:
+                                        center_time=time.strptime(tpdu.time_stamp,"%y%m%d%H%M%S")
+                                    except:
+                                        center_time=time.localtime()
+                                    print("%s (Downlink) [MMSE from %s] " % (time.strftime("%Y-%m-%d %H:%M:%S", center_time),tpdu.TP_origin))
                             # SMS-STATUS REPORT
                             if tpdu.TP_mti == 0x02:
+                                try:
+                                        center_time=time.strptime(tpdu.time_stamp,"%y%m%d%H%M%S")
+                                except:
+                                        center_time=time.localtime()
                                 print(
-                                    "[Downlink ]SMS status report from %s] " % tpdu.TP_origin)
+                                    "%s (Downlink) [SMS status report from %s] " % (time.strftime("%Y-%m-%d %H:%M:%S", center_time),tpdu.TP_origin))
                                 # if tpdu.status_result == 0:
                                 #     print(
                                 #         "[Downlink ]SMS status report from %s] Short message transaction completed, Short message received by the SME" % tpdu.TP_origin)
@@ -64,22 +78,29 @@ def handle_message(**kargs):
                                 #         "[Downlink ]SMS status report from %s] Short message transaction completed, Short message forwarded by the SC to the SME but the SC is unable to confirm delivery" % tpdu.TP_origin)
                         # Message Type RP-DATA (MS to Network)
                         elif rp.RP_message_type == 0:
-                            tpdu = TPDU(rp.next_data)
+                            try:
+                                tpdu = TPDU(rp.next_data)
+                            except:
+                                hexdump.hexdump(dtap.next_data)
+                                hexdump.hexdump(rp.next_data)
+                            print "-"*90
+                            center_time=time.localtime()
                             # SMS-SUBMIT
                             if tpdu.TP_mti == 0x01:
                                 if tpdu.TP_charaterset == 2:
-                                    try:
-                                        print("[SMS from %s] %s" % (
-                                            tpdu.TP_origin, tpdu.data.decode("utf-16be").encode("utf-8")))
-                                    except:
-                                        print(
-                                            "Decode Failed!Maybe wrong format")
+                                    center_time=time.localtime()
+                                    print("%s (Uplink)[SMS to %s] %s" % (
+                                            time.strftime("%Y-%m-%d %H:%M:%S", center_time),tpdu.TP_dest, tpdu.data.decode("utf-16be").encode("utf-8")))
                                 else:
-                                    print("MMSE uplink")
+                                    center_time=time.localtime()
+                                    print("%s (Uplink) MMSE uplink" % time.strftime("%Y-%m-%d %H:%M:%S", center_time))
                         # RP-ACK
                         elif rp.RP_message_type == 2:
                             # SMS-DELIVER REPORT
-                            print("SMS status report uplink")
+                            print "-"*90
+                            ceter_time=time.localtime()
+                            print("%s (Uplink) SMS status report" % time.strftime("%Y-%m-%d %H:%M:%S", center_time))
+
 
                 # print("LINK[%d] ARFCN=%d TIME_SLOT=%d CHANNEL=%d, N(R)=%d N(S)=%d, segment more[%d], payload len=%d\n" %
                 #   (gsmtap.link, gsmtap.arfcn, gsmtap.time_slot, gsmtap.channel_type, lapdm.n_r, lapdm.n_s, lapdm.last_segment, lapdm.length))
